b409a2a627772d19bf9c54d5687393391e1521df
Ander6767 <andersantiagoinfantemontiel@gmail.com>
2026-06-11 10:11:42 -0600

    Corregir servidor y puerto

--- server.js
+++ server.js
@@ -19,3 +19,40 @@ mongoose.connect(process.env.MONGO_URI)
     console.error("❌ Error completo:");
     console.error(err);
 });
+
+// Esquema
+const ProductoSchema = new mongoose.Schema({
+    nombre: String,
+    precio: Number,
+    existencia: Number
+});
+
+const Producto = mongoose.model('Producto', ProductoSchema);
+
+// Ruta de prueba
+app.get('/', (req, res) => {
+    res.send('Servidor funcionando');
+});
+
+// Obtener productos
+app.get('/productos', async (req, res) => {
+    const productos = await Producto.find();
+    res.json(productos);
+});
+
+// Guardar producto
+app.post('/productos', async (req, res) => {
+    const nuevoProducto = new Producto(req.body);
+    await nuevoProducto.save();
+    res.json({
+        mensaje: "Producto registrado",
+        nuevoProducto
+    });
+});
+
+// Iniciar servidor
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor activo en puerto ${PORT}`);
+});
